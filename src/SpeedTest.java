b34f7f279f34e35eff1d04c32f145e3fdb5bfbfb
qkswj1248 <gksdmswls123@naver.com>
2022-09-28 21:35:13 +0900

    second commit

--- src/SpeedTest.java
+++ src/SpeedTest.java
@@ -1,14 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class SpeedTest {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] num = br.readLine().split(" ");
-        BigInteger a = new BigInteger(num[0]);
-        BigInteger b = new BigInteger(num[1]);
-        System.out.println(a.add(b));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+
     }
 }
--- src/math/Factorization.java
+++ src/math/Factorization.java
@@ -2,50 +2,58 @@ package math;
 
 import java.io.*;
 
+/**
+ *  백준 11653 : 소인수분해
+ *  (1은 출력안함)
+ *  - 입력 -
+ *  72
+ *  - 출력 -
+ *  2
+ *  2
+ *  2
+ *  3
+ *  3
+ */
+/*
+    내가 푼 방법
+    -> 1을 먼저 빼고 73같은 소수는 1씩 증가하면서 검사하면 비효율 적이니까
+    따로 함수에서 i*i<=n으로 검사하고 소수가 아닌 건 반복문 돌려가면서 나누기
+    다른 사람이 푼 방법
+    -> 처음부터 반복문을 빠르게 돌릴 수 있도록 i*i<=j을 쓰기 왜 이걸 써도 되냐면
+    소인수분해는 소수로만 나누기때문. 그리고 i*i<=j는 i<=루트j와 같음
+ */
 public class Factorization {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println(findFactorization(n));
-    }
-
-    private static StringBuilder findFactorization(int n) {
-        StringBuilder sb = new StringBuilder();
         if(n==1){
-            return sb.append("");
-        }else if(n==2){
-            return sb.append(2);
-        }
-        
-
-        return sb;
-    }
-
-    private static int[] findDecimal(int n) {
-        if(n==1){
-            return null;
-        }
-
-        int decimal[] = new int[n/2];
-        int count = 1, key = 0;
-        decimal[0] = 2;
-
-        if(n==2){
-            return decimal;
+        }else if(isDecimal(n)){
+            System.out.println(n);
         }else{
-            for(int i=3; i<=n; i++){
-                for(int j=2; j*j<=i; j++){
-                    if(i%j==0){
-                        key = 1;
-                        break;
-                    }
-
+            int num = n;
+            for(int i=2; i<=(n/2); i++){
+                while(num%i==0){
+                    num = num/i;
+                    System.out.println(i);
                 }
             }
+
         }
-        return decimal;
+
     }
 
+    public static boolean isDecimal(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
